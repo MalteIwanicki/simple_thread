6b4895dcdceeac5c5ac371222b8de98fd41796e2
Malte Iwanicki <malteiwa@gmail.com>
2021-08-28 10:34:31 +0200

    struktural changes

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='simple_thread',
-   version='0.1',
-   author='Malte Iwanicki',
-   author_email='malteiwa@gmail.com',
-   packages=['simple_thread', 'simple_thread.test'],
-   description='A simple way to start a function in a thread',
-   long_description=open('README.txt').read(),
-   install_requires=[
-       "pytest"
-   ],
+    name="simple_thread",
+    version="0.1",
+    description="Makes running a function in a thread simple",
+    author="Malte Iwanicki",
+    author_email="malteiwa@gmail.com",
+    url="https://github.com/MalteIwanicki/simple_thread",
+    packages=["simple_thread"],
 )
--- simple_thread/simple_thread.py
+++ simple_thread/simple_thread.py
@@ -1,7 +1,8 @@
 from threading import Thread
 
+
 class SimpleThread(Thread):
-    """ Creates and starts a thread with the given function and parameter """
+    """Creates and starts a thread with the given function and parameter"""
 
     def __init__(self, function, parameters=()):
         if not isinstance(parameters, tuple):
@@ -10,11 +11,9 @@ class SimpleThread(Thread):
         self._return = None
         self.start()
 
-
     def run(self):
         if self._target is not None:
-            self._return = self._target(*self._args,
-                                                **self._kwargs)
+            self._return = self._target(*self._args, **self._kwargs)
 
     def join(self, *args):
         Thread.join(self, *args)
